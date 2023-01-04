18d81a78fa93e3abd2c49d3b2ac93ec32296b8a0
prava <prava@HP>
2023-01-04 12:43:23 +0530

    added print statement

---
+++ GitProject/src/gitPackage/A.java
@@ -0,0 +1,12 @@
+package gitPackage;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("Selenium");
+		System.out.println("Automation");
+		System.out.println("testing");
+	}
+	
+
+}
